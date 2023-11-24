581800644981e37fb34747c0be5749494c443fed
Samrudhi <ashishsuresh@ashishs-MacBook-Air-2.local>
2023-11-24 19:00:48 +0000

    add static , templates dir and basic Frontend code

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask , render_template
 from api.controllers.recipe_controller import recipe_controller
 
 app = Flask(__name__)
 app.register_blueprint(recipe_controller)
+
+@app.route('/')
+def helloWorld() :
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
